efeb5903b2412aef4a69dde393fff47cce45fbc7
Idan <idanfadloneng@gmail.com>
2022-03-20 00:21:29 +0200

    add server side and fix navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/TopNavBar.css';
 import TopNavBar from './components/TopNavBar';
 import BottomFooter from './components/BottomFooter';
+// import { Switch, Route, HashRouter } from "react-router-dom";
 
 function App() {
   return (
   <div>
-    <TopNavBar/>
-    <BottomFooter/>
+    <TopNavBar />
+    <BottomFooter />
   </div>
   );
 }
---
+++ src/components/TopNavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Navbar,Nav} from 'react-bootstrap';
+import '../css/TopNavBar.css';
+
+const TopNavBar=()=>{
+    return (
+  <Navbar collapseOnSelect className='my-navbar' expand="lg" variant="dark">
+  <Navbar.Brand className="logo me-auto p-2" href="#home">CFN-Brand</Navbar.Brand>
+    <Nav>
+      <Nav.Link className="my-nav-link p-3" href="#loginregister">Login & Register</Nav.Link>
+     
+    </Nav>
+  
+</Navbar>
+    );
+}
+
+export default TopNavBar;
