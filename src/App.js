dc05b07ab157c58b8b123457d9ffacd9ba3d3e40
Asaf Navon <asnav1999@gmail.com>
2022-03-20 20:55:33 +0200

    eslint reinstalled and initialized

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import TopNavBar from './components/TopNavBar';
 import BottomFooter from './components/BottomFooter';
 import HomePage from './components/Home/HomePage';
 import LoginPage from './components/Login/LoginPage';
-import { BrowserRouter ,Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import React from 'react';
 
-
+/**
+ * constructs the base of the app
+ * @return {JSX.Element}
+ */
 function App() {
   return (
-  <div>
-    <TopNavBar />
-    <BrowserRouter>
-    <Routes>
-    <Route exact path='/' element={<HomePage />}/>
-    <Route path='/Login' element={<LoginPage />}/>
-    </Routes>
-    </BrowserRouter>
-    <BottomFooter />
-  </div>
+    <div>
+      <TopNavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<HomePage />}/>
+          <Route path='/Login' element={<LoginPage />}/>
+        </Routes>
+      </BrowserRouter>
+      <BottomFooter />
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import React from 'react';
+
 
 test('renders login link', () => {
   render(<App />);
-  const linkElement = screen.getByText("Login");
+  const linkElement = screen.getByText('Login');
   expect(linkElement).toBeInTheDocument();
 });
