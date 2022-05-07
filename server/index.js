5a03233aad5bab958480473468aa1ab0d69b6863
Asaf Navon <asnav1999@gmail.com>
2022-05-07 22:39:29 +0300

    Cleared react create-app unnecessary files and replaced the react favicon with an 'm' favicon

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
 });
 
 app.listen(port, () => {
