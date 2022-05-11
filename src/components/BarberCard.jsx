e917a6a8e77c764354a499cfff58e1f60896022b
OmerAlali2020 <omeralali@gmail.com>
2022-05-11 10:13:44 +0300

    ReCommit AppointmentsTypes, BarberCard, BarberPersonalPage, ClientAppointments that delited from the repo in the last commit

---
+++ src/components/BarberCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './images/barber.png';
+
+export default function BarberCard() {
+  const list = [
+    {
+      id: 1,
+      name: 'Robin',
+      specialization: 'Beards',
+    },
+    {
+      id: 2,
+      name: 'Dennis',
+      specialization: 'Hairstyles',
+    },
+  ];
+
+  const imageName = require('./barber.png');
+
+  function handleRemove(id) {}
+
+  return (
+    <div className="mainBoxx">
+      <ul>
+        {list.map((item) => (
+          <div className="cardbox" key={item.id}>
+            <img src={imageName} alt="baberimage" />
+            <div className="container">
+              <h4>{item.name}</h4>
+              <p>{item.specialization}</p>
+              <button type="button" onClick={() => handleRemove(item.id)}>
+                Pick time
+              </button>
+            </div>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
