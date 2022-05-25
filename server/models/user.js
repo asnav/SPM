3671a22c3910c17d5e82c8d3423bdc28d4279e83
Idan <idanfadloneng@gmail.com>
2022-05-25 15:46:00 +0300

    update user schema

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   fullname: {type: String, required: true},
   password: {type: String, required: true},
   isBarber: {type: Boolean, default: false, required: true},
+  isOwner: {type: Boolean, default: false, required: true},
 },
 );
 const User = mongoose.model('User', UserSchema);
